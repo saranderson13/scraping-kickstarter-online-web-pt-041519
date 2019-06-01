de68b36bd2fea87cfeb0f303ffa3d405c8583e7e
github username <github email address>
2019-06-01 15:33:50 -0400

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  # projects:     kickstarter.css("li.project.grid_4")
+  # projects:     (project=) kickstarter.css("li.project.grid_4")
   # title:        project.css("h2.bbcard_name strong a").text
   # image:        project.css("div.project-thumbnail a img").attribute("src").value
   # description:  project.css("p.bbcard_blurb").text
